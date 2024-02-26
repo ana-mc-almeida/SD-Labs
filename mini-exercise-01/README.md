9d6717f686df51f1be08d376539fef18c217a398
Ana Margarida Almeida <ana.margarida.almeida@tecnico.ulisboa.pt>
2024-02-26 16:18:47 +0000

    Add mini-exercise-01-invalid_play

--- mini-exercise-01/README.md
+++ mini-exercise-01/README.md
@@ -5,3 +5,4 @@
 Alterar o [código base do Lab-04](../lab-04-rpc-error/exercise_ttt-grpc-master/) para:
 - [Quando o cliente escolher o número 10, apenas trocar de player sem realizar nenhuma jogada](./exercise_ttt-change_player/)
 - Desfazer a jogada anterior
+- Fazer uma anteração **no play** para quando o client selecionar `-1` obter uma mensagem de `INVALID_PLAY`
---
+++ mini-exercise-01/exercise_ttt-invalid_play/server/src/main/java/pt/tecnico/ttt/server/TTTServiceImpl.java
@@ -0,0 +1,61 @@
+package pt.tecnico.ttt.server;
+
+import io.grpc.stub.StreamObserver;
+import pt.tecnico.ttt.*;
+
+import static io.grpc.Status.INVALID_ARGUMENT;
+
+public class TTTServiceImpl extends TTTGrpc.TTTImplBase {
+
+	/** Game implementation. */
+	private TTTGame ttt = new TTTGame();
+
+	@Override
+	public void currentBoard(CurrentBoardRequest request, StreamObserver<CurrentBoardResponse> responseObserver) {
+		// StreamObserver is used to represent the gRPC stream between the server and
+		// client in order to send the appropriate responses (or errors, if any occur).
+
+		CurrentBoardResponse response = CurrentBoardResponse.newBuilder().setBoard(ttt.toString()).build();
+
+		// Send a single response through the stream.
+		responseObserver.onNext(response);
+		// Notify the client that the operation has been completed.
+		responseObserver.onCompleted();
+	}
+
+	@Override
+	public void play(PlayRequest request, StreamObserver<PlayResponse> responseObserver){
+
+		PlayResult result = ttt.play(request.getRow(), request.getColumn(), request.getPlayer());
+
+		if (result == PlayResult.OUT_OF_BOUNDS){
+			responseObserver.onError(INVALID_ARGUMENT.withDescription("Input has to be a valid position").asRuntimeException());
+		}
+		else{
+			// Send a single response through the stream.
+			PlayResponse response = PlayResponse.newBuilder().setPlayResult(result).build();
+			responseObserver.onNext(response);
+			// Notify the client that the operation has been completed.
+			responseObserver.onCompleted();
+		}
+	}
+
+	@Override
+	public void checkWinner(CheckWinnerRequest request, StreamObserver<CheckWinnerResponse> responseObserver){
+
+		CheckWinnerResponse response = CheckWinnerResponse.newBuilder().setResult(ttt.checkWinner()).build();
+
+		responseObserver.onNext(response);
+		responseObserver.onCompleted();
+	}
+
+	@Override 
+	public void waitForWinner(WaitForWinnerRequest request, StreamObserver<WaitForWinnerResponse> responseObserver){
+
+		WaitForWinnerResponse response = WaitForWinnerResponse.newBuilder().setResult(ttt.waitForWinner()).build();
+
+		responseObserver.onNext(response);
+		responseObserver.onCompleted();
+	}
+
+}
